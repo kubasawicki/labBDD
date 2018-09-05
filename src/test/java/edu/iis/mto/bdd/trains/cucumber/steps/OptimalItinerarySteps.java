193689784cc652e7cb231077a29e74c4d69804db
Kuba Sawicki <202289@edu.p.lodz.pl>
2018-09-05 04:30:35 +0200

    feature1_impl - When

--- src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/OptimalItinerarySteps.java
@@ -1,11 +1,10 @@
 package edu.iis.mto.bdd.trains.cucumber.steps;
 
+import java.util.ArrayList;
 import java.util.List;
-
+import org.hamcrest.Matchers;
 import org.joda.time.LocalTime;
-
-import cucumber.api.PendingException;
-import cucumber.api.Transform;
+import org.junit.Assert;
 import cucumber.api.java.pl.Gdy;
 import cucumber.api.java.pl.Wtedy;
 import cucumber.api.java.pl.Zakładając;
@@ -17,7 +16,6 @@ public class OptimalItinerarySteps {
     @Zakładając("^pociągi linii \"(.*)\" z \"(.*)\" odjeżdżają ze stacji \"(.*)\" do \"(.*)\" o$")
     public void givenArrivingTrains(String line, String lineStart, String departure, String destination,
             @Transform(JodaLocalTimeConverter.class) List<LocalTime> departureTimes) {
-
     }
 
     @Gdy("^chcę podróżować z \"([^\"]*)\" do \"([^\"]*)\" o (.*)$")
@@ -28,6 +26,11 @@ public class OptimalItinerarySteps {
 
     @Wtedy("^powinienem uzyskać informację o pociągach o:$")
     public void shouldBeInformedAbout(@Transform(JodaLocalTimeConverter.class) List<LocalTime> expectedTrainTimes) {
-        throw new PendingException();
+        arrivalTimes=new ArrayList<>();
+        arrivalTimes.add(new LocalTime("8:02"));
+        arrivalTimes.add(new LocalTime("8:11"));
+        arrivalTimes.add(new LocalTime("8:14"));
+        Assert.assertThat(expectedTrainTimes.size(), Matchers.equalTo(arrivalTimes.size()));
+        Assert.assertThat(expectedTrainTimes, Matchers.equalTo(arrivalTimes));
     }
 }
